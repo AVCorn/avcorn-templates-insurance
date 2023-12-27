aa319279be872207aebd2e3a30dfe58ea6c16877
Benjamin J. Young <ben@blaher.me>
2023-12-27 08:44:30 -0500

    Initial template files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# avcorn-templates-insurance
-AVCorn Insurance Template
+# AVCorn Insurance Template
+
+![Template Preview](assets/images/previews/preview-lg.png "Insurance Design Preview")
+
+An insurance company template.
+
+
+
+[< Back to AVCorn Templates](https://github.com/AVCorn/avcorn-templates)
+
+[<< Back to AVCorn Project](https://github.com/AVCorn/avcorn)
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * New Template Configuration
+ *
+ * PHP version 8.2
+ *
+ * @phpversion >= 8.2
+ * @category   CMS
+ * @package    AVCorn
+ * @subpackage Template
+ * @author     Benjamin J. Young <ben@blaher.me>
+ * @license    GNU General Public License, version 3
+ * @link       https://github.com/avcorn/avcorn
+ */
+
+declare(strict_types=1);
+
+/**
+ * Overriding default configuration.
+ *
+ * @var array $config Configuration
+ */
+$config['info']['company'] = 'Oak Insurance';
+$config['info']['title'] = 'All Your Insurance "Needs"';
+$config['info']['motto'] = 'It would be wise to get some "insurance".';
+$config['info']['slogan'] = 'GET SOME INSURANCE!!!';
